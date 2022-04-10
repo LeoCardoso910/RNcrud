22764c3577918c782e03ff60af31171778a882da
Leocendino Cardoso <ncardosoleo@gmail.com>
2022-04-10 20:09:50 -0300

    Cadastro simples de Usuários

---
+++ src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    name: 'João',
+    email: 'joao@gmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375__340.png',
+  },
+  {
+    id: 2,
+    name: 'Ivany',
+    email: 'ivany@gmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
+  },
+  {
+    id: 3,
+    name: 'Andressa',
+    email: 'andressa@gmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
+  },
+  {
+    id: 4,
+    name: 'Rafael',
+    email: 'Rafael@gmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
+  },
+];
---
+++ src/views/UserList.js
@@ -0,0 +1,67 @@
+import {ListItem, Avatar, Button, Icon} from '@rneui/base';
+import React, {useContext} from 'react';
+import {View, Text, FlatList, Alert} from 'react-native';
+import UsersContext from '../context/UsersContext';
+
+export default props => {
+  const {state, dispatch} = useContext(UsersContext);
+
+  function confirmUserDeletion(user) {
+    Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+      {
+        text: 'Sim',
+        onPress() {
+          dispatch({
+            type: 'deleteUser',
+            payload: user,
+          });
+        },
+      },
+      {
+        text: 'Não',
+      },
+    ]);
+  }
+
+  function getAction(user) {
+    return (
+      <View style={{flexDirection: 'row'}}>
+        <Button
+          onPress={() => props.navigation.navigate('UserForm', user)}
+          type="clear"
+          icon={<Icon name="edit" size={25} color="orange" />}
+        />
+        <Button
+          onPress={() => confirmUserDeletion(user)}
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
+        />
+      </View>
+    );
+  }
+
+  function getUserItem({item: user}) {
+    return (
+      <ListItem
+        bottomDivider
+        onPress={() => props.navigation.navigate('UserForm', user)}>
+        <Avatar source={{uri: user.avatarUrl}} />
+        <ListItem.Content key={user.id}>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+        <ListItem.Input rightIcon={getAction(user)} />
+      </ListItem>
+    );
+  }
+
+  return (
+    <View>
+      <FlatList
+        keyExtractor={user => user.id.toString()}
+        data={state.users}
+        renderItem={getUserItem}
+      />
+    </View>
+  );
+};
